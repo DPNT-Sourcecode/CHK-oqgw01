61c8a1efefac5db580adbc6323d2bf4da049b209
sbx_user1051 <sbx_user1051@169.254.220.189>
2020-02-07 14:58:41 +0000

    Fri Feb 07 14:58:41 UTC 2020

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,4 +2,6 @@
 def compute(x, y):
     if x > 100 or y > 100:
         raise ValueError("Too high value")
+    if not isinstance(x, int) or not isinstance(y, int):
+        raise ValueError("Error")
     return x + y
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -4,3 +4,8 @@ from solutions.SUM import sum_solution
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+    def test_sum(self):
+        assert sum_solution.compute("101", 2)
+
+

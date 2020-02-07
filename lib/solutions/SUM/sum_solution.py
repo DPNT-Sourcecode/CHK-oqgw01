d32edbe9ca0477b38b4902060c1593cf783275a1
sbx_user1051 <sbx_user1051@169.254.220.189>
2020-02-07 14:56:27 +0000

    Fri Feb 07 14:56:27 UTC 2020

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,4 +1,5 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    # raise NotImplementedError()
+    if x > 100 or y > 100:
+        raise ValueError("Too high value")
     return x + y
